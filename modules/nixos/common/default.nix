b407353e0e96c97d9ac0715acfb6177ba70e19e0
Zuzanna <zuzanna23@proton.me>
2025-09-14 20:23:29 +0200

    droidcam fix

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -1,16 +1,22 @@
-{ inputs, lib, config, pkgs, ... }: {
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
 
- imports = [
-  ./hardware-configuration.nix
-  ../desktop/hyprland
-  ../drivers
-  ../programs
-  ../services
-  ../misc
+  imports = [
+    ./hardware-configuration.nix
+    ../desktop/hyprland
+    ../drivers
+    ../programs
+    ../services
+    ../misc
   ];
 
-  environment.systemPackages = with pkgs; 
-  [
+  environment.systemPackages = with pkgs; [
     git
     wget
     neovim
@@ -23,31 +29,37 @@
   ];
   nixpkgs.config.allowUnfree = true;
 
-  nix = let
-    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
-  in {
-    settings = {
-      # Enable flakes and new 'nix' command
-      experimental-features = "nix-command flakes";
-      # Opinionated: disable global registry
-      flake-registry = "";
-      # Workaround for https://github.com/NixOS/nix/issues/9574
-      nix-path = config.nix.nixPath;
+  nix =
+    let
+      flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+    in
+    {
+      settings = {
+        # Enable flakes and new 'nix' command
+        experimental-features = "nix-command flakes";
+        # Opinionated: disable global registry
+        flake-registry = "";
+        # Workaround for https://github.com/NixOS/nix/issues/9574
+        nix-path = config.nix.nixPath;
 
+      };
+      # Opinionated: disable channels
+      channel.enable = false;
 
+      # Opinionated: make flake registry and nix path match flake inputs
+      registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
+      nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
     };
-    # Opinionated: disable channels
-    channel.enable = false;
-
-    # Opinionated: make flake registry and nix path match flake inputs
-    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
-  };
 
   users.users = {
     sayu = {
       isNormalUser = true;
-      extraGroups = ["networkmanager" "wheel" "input"];
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+        "input"
+        "video"
+      ];
     };
   };
 
@@ -56,11 +68,11 @@
 
   # Bootloader.
   boot.loader.grub.enable = true;
-#  boot.loader.grub.device = "/dev/nvme0n1p4";
-  boot.loader.grub.useOSProber = true; 
+  #  boot.loader.grub.device = "/dev/nvme0n1p4";
+  boot.loader.grub.useOSProber = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.devices = [ "nodev" ];  
+  boot.loader.grub.devices = [ "nodev" ];
 
   # Set your time zone.
   time.timeZone = "Europe/Warsaw";
@@ -79,7 +91,6 @@
     LC_TELEPHONE = "pl_PL.UTF-8";
     LC_TIME = "pl_PL.UTF-8";
   };
-
 
   # Audio
   services.pulseaudio.enable = false;
@@ -104,9 +115,6 @@
   # Configure console keymap
   console.keyMap = "pl2";
 
-
   system.stateVersion = "24.11"; # It's perfectly fine and recommended to leave this value at the release version of the first install of this system.
-
-  
 
 }
--- modules/nixos/programs/droidcam.nix
+++ modules/nixos/programs/droidcam.nix
@@ -3,4 +3,11 @@
   programs.droidcam = {
     enable = true;
   };
+
+  # Droidcam kernel modules
+  boot.kernelModules = [ "v4l2loopback" ];
+  boot.extraModulePackages = [ pkgs.linuxPackages.v4l2loopback ];
+  boot.extraModprobeConfig = ''
+  options v4l2loopback devices=1 video_nr=0 card_label="DroidCam" exclusive_caps=1
+'';
 }
