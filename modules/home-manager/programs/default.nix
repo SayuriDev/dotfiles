f77b8206a5eb7ab405066999d627ebfd1ee78484
SayuriDev <75377594+SayuriDev@users.noreply.github.com>
2025-09-15 11:57:52 +0200

    add gimp

--- modules/home-manager/programs/default.nix
+++ modules/home-manager/programs/default.nix
@@ -1,16 +1,17 @@
-{...}:
+{ ... }:
 {
   imports = [
-  ./firefox.nix
-  ./prismlauncher.nix
-  ./vscode.nix
-  ./discord.nix
-  ./spotify.nix
-  ./kitty.nix
-  ./btop.nix
-  ./wofi.nix
-  ./nixvim.nix
-  ./yazi.nix
-  ./helix.nix
+    ./firefox.nix
+    ./prismlauncher.nix
+    ./vscode.nix
+    ./discord.nix
+    ./spotify.nix
+    ./kitty.nix
+    ./btop.nix
+    ./wofi.nix
+    ./nixvim.nix
+    ./yazi.nix
+    ./helix.nix
+    ./gimp.nix
   ];
 }
