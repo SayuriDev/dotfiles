fa2b374f7d340ea900b1313e1a5407cbd9633060
SayuriDev <75377594+SayuriDev@users.noreply.github.com>
2025-10-03 09:31:48 +0200

    xdg

--- modules/home-manager/misc/xdg.nix
+++ modules/home-manager/misc/xdg.nix
@@ -2,32 +2,43 @@
 {
   xdg = {
     enable = true;
+
     mimeApps = {
       enable = true;
-      associations.added = {
-        "audio/mpeg" = [ "io.mpv.Mpv.desktop" ];
-        "image/jpeg" = [ "org.kde.gwenview.desktop" ];
-        "image/png" = [ "org.kde.gwenview.desktop" ];
-        "video/mp3" = [ "io.mpv.Mpv.desktop" ];
-        "video/mp4" = [ "io.mpv.Mpv.desktop" ];
-        "video/quicktime" = [ "io.mpv.Mpv.desktop" ];
-        "video/webm" = [ "io.mpv.Mpv.desktop" ];
-      };
+
       defaultApplications = {
-        #   "application/json" = ["gnome-text-editor.desktop"];
-        #   "application/toml" = ["org.gnome.TextEditor.desktop"];
-        "audio/*" = [ "io.mpv.Mpv.desktop" ];
-        "audio/mp3" = [ "io.mpv.Mpv.desktop" ];
-        "image/*" = [ "org.kde.gwenview.desktop" ];
+        "application/zip"             = [ "org.kde.ark.desktop" ];
+        "application/x-tar"           = [ "org.kde.ark.desktop" ];
+        "application/x-7z-compressed" = [ "org.kde.ark.desktop" ];
+        "application/x-rar"           = [ "org.kde.ark.desktop" ];
+        "application/vnd.rar"         = [ "org.kde.ark.desktop" ];
+        "application/x-bzip2"         = [ "org.kde.ark.desktop" ];
+        "application/x-gzip"          = [ "org.kde.ark.desktop" ];
+        "application/x-xz"            = [ "org.kde.ark.desktop" ];
+
+        "audio/mpeg"   = [ "io.mpv.Mpv.desktop" ]; # mp3
+        "audio/mp4"    = [ "io.mpv.Mpv.desktop" ]; # m4a
+        "audio/x-wav"  = [ "io.mpv.Mpv.desktop" ]; # wav
+        "audio/flac"   = [ "io.mpv.Mpv.desktop" ];
+        "audio/ogg"    = [ "io.mpv.Mpv.desktop" ];
+        "audio/x-opus+ogg" = [ "io.mpv.Mpv.desktop" ];
+
+        "video/mp4"        = [ "io.mpv.Mpv.desktop" ];
+        "video/webm"       = [ "io.mpv.Mpv.desktop" ];
+        "video/x-matroska" = [ "io.mpv.Mpv.desktop" ]; # mkv
+        "video/quicktime"  = [ "io.mpv.Mpv.desktop" ];
+        "video/x-msvideo"  = [ "io.mpv.Mpv.desktop" ]; # avi
+        "video/x-flv"      = [ "io.mpv.Mpv.desktop" ];
+
         "image/jpeg" = [ "org.kde.gwenview.desktop" ];
-        "image/png" = [ "org.kde.gwenview.desktop" ];
-        #   "text/plain" = "org.gnome.TextEditor.desktop";
-        "video/*" = [ "io.mpv.Mpv.desktop" ];
-        "video/mp4" = [ "io.mpv.Mpv.desktop" ];
-        "application/zip" = [ "org.kde.ark.desktop" ];
-        "application/vnd.rar" = [ "org.kde.ark.desktop" ];
+        "image/png"  = [ "org.kde.gwenview.desktop" ];
+        "image/gif"  = [ "org.kde.gwenview.desktop" ];
+        "image/webp" = [ "org.kde.gwenview.desktop" ];
+        "image/svg+xml" = [ "org.kde.gwenview.desktop" ];
+        "image/tiff" = [ "org.kde.gwenview.desktop" ];
       };
     };
+
     userDirs = {
       enable = true;
       createDirectories = true;
